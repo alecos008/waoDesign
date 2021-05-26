2d5c7fe745c744c92b68b6c456d1a019b0a447cc
Alejandro Cazorla <acazorla72@gmail.com>
2021-05-26 22:46:45 +0200

    declared p and h4 variables and fixed mOver mOut functions

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
-let socialMediaBox = document.getElementById('socialMediaBox');
-let socialMediaLogo = document.getElementById('socialMediaLogo');
-let socialMediaHeading = document.getElementById('socialMediaHeading');
-let socialMediaText = document.getElementById('socialMediaText');
+let boxes = document.getElementsByClassName('box');
 
-const socialMediaLogoImg = socialMediaLogo.innerHTML;
-const socialMediaHeadingText = socialMediaHeading.innerHTML.fontsize('5em');
-const socialMediaInnerText = socialMediaText.innerHTML.fontsize('4em');
+let designBox = boxes[0];
+let designTextElement = designBox.querySelector('p')
+let designHeadingElement = designBox.querySelector('h4');
 
-function mOver (obj) {
-    obj.innerHTML = socialMediaInnerText;
-    
-}
-function mOut (obj) {
-    obj.innerHTML = socialMediaLogoImg + socialMediaHeadingText;
-}
+//got UX/UI Web Design inner p text
+const designInnerText = designTextElement.innerHTML;
+//got UX/UI Web design inner h4 text
+const designHeadingText = designHeadingElement.innerHTML;
+
+let marketingBox = boxes[1];
+let marketingTextElement = marketingBox.querySelector('p');
+let marketingHeadingElement =  marketingBox.querySelector('h4');
+
+//got SEO and Marketing inner p text
+const marketingInnerText = marketingTextElement.innerHTML;
+//got SEO and Marketing inner h4 text
+const marketingHeadingText = marketingHeadingElement.innerHTML;
+
+let mediaBox = boxes[2];
+let mediaTextElement = mediaBox.querySelector('p');
+let mediaHeadingElement =  mediaBox.querySelector('h4');
+
+//got SMM inner p text
+const mediaInnerText = mediaTextElement.innerHTML;
+//got SMM inner h4 text
+const mediaHeadingText = mediaHeadingElement.innerHTML;
+
+
+const mediaBlock = mediaHeadingText + mediaInnerText;
+
+
+function mOver(obj) {
+    if (obj.id === "socialMediaBox"){
+    obj.innerHTML = mediaHeadingText.fontsize('5em') + mediaInnerText.fontsize('3em');
+      } else if (obj.id === "marketingBox") {
+        obj.innerHTML = marketingHeadingText.fontsize('5em') + marketingInnerText.fontsize('3em');
+      } else if (obj.id === "designBox") {
+        obj.innerHTML = designHeadingText.fontsize('5em') + designInnerText.fontsize('3em');
+      }
+  };
+  
+  function mOut(obj) {
+     if (obj.id === "socialMediaBox"){
+    obj.innerHTML = mediaHeadingText.fontsize('5em');
+      } else if (obj.id === "marketingBox") {
+        obj.innerHTML = marketingHeadingText.fontsize('5em');
+      } else if (obj.id === "designBox") {
+        obj.innerHTML = designHeadingText.fontsize('5em');
+      }
+  };
